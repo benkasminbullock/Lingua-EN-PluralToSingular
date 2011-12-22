944a0287d2cc6b740387ca377224329f00a19521
Ben Bullock <benkasminbullock@gmail.com>
2011-12-22 10:01:32 +0900

    Added minimal functionality and test from cpanratings review.

--- lib/Lingua/EN/PluralToSingular.pm
+++ lib/Lingua/EN/PluralToSingular.pm
@@ -1,3 +1,152 @@
+package Lingua::EN::PluralToSingular;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw/to_singular/;
+use warnings;
+use strict;
+our $VERSION = 0.01;
+
+# Irregular plurals.
+
+my %irregular = (
+    'women' => 'woman',
+    'mice' => 'mouse',
+    'men' => 'man',
+    'children' => 'child',
+    'geese' => 'goose',
+    'feet' => 'foot',
+    'teeth' => 'tooth',
+    'lice' => 'louse',
+);
+
+# Words ending in ves need care, since the ves may become "f" or "fe".
+
+my %ves = (
+    'wolves' => 'wolf',
+    'knives' => 'knife',
+);
+
+# A dictionary of plurals.
+
+my %plural = (
+    # Words ending in "us" which are plural, in contrast to words like
+    # "citrus" or "bogus".
+    'menus' => 'menu',
+    %ves,
+    %irregular,
+);
+
+
+# Words which are the same in both singular and plural.
+
+my @no_change = qw/
+deer
+fish
+means
+offspring
+series
+sheep
+species
+/;
+
+@plural{@no_change} = @no_change;
+
+# Store of words which look like plurals but are not.
+
+my @not_plural = (qw/
+    citrus
+    Charles
+    bogus
+/);
+
+my %not_plural;
+
+@not_plural{@not_plural} = (1) x @not_plural;
+
+# Store of words which end in oe and whose plural ends in oes.
+
+my @oes = (qw/
+                 toe
+                 hoe
+             /);
+
+my %oes;
+
+@oes{@oes} = (1) x @oes;
+
+# Store of words which end in ie and whose plural ends in ies.
+
+my @ies = (qw/
+                 lie
+             /);
+
+my %ies;
+
+@ies{@ies} = (1) x @ies;
+
+# A regular expression which matches the end of words like "dishes"
+# and "sandwiches". $1 is a capture which contains the part of the
+# word which should be kept in a substitution.
+
+my $es_re = qr/(ch|sh)es$/;
+
+sub to_singular
+{
+    my ($word) = @_;
+    # The return value.
+    my $singular = $word;
+    if (! $not_plural{$word}) {
+        # The word is not in the list of exceptions.
+        if ($plural{$word}) {
+            # The word has an irregular plural, like "children", or
+            # "geese", so look up the singular in the table.
+            $singular = $plural{$word};
+        }
+        elsif ($word =~ /s$/) {
+            # The word ends in "s".
+            if ($word =~ /ies$/) {
+                # The word ends in "ies".
+                if ($ies{"${word}s"}) {
+                    # Lies -> lie
+                    $singular =~ s/ies$/ie/;
+                }
+                else {
+                    # Fries -> fry
+                    $singular =~ s/ies$/y/;
+                }
+            }
+            elsif ($word =~ /oes/) {
+                # The word ends in "oes".
+                if ($oes{"${word}s"}) {
+                    # Toes -> toe
+                    $singular =~ s/oes$/oe/;
+                }
+                else {
+                    # Potatoes -> potato
+                    $singular =~ s/oes$/o/;
+                }
+            }
+            elsif ($word =~ $es_re) {
+                # Sandwiches -> sandwich
+                # Dishes -> dish
+                $singular =~ s/$es_re/$1/;
+            }
+            else {
+                # Now the program has checked for every exception it
+                # can think of, so it assumes that it is OK to remove
+                # the "s" from the end of the word.
+                $singular =~ s/s$//;
+            }
+        }
+    }            
+    return $singular;
+}
+
+1;
+
+
+__END__
+
 =head1 NAME
 
 Lingua::EN::PluralToSingular - change an English plural to a singular
@@ -9,28 +158,27 @@ Lingua::EN::PluralToSingular - change an English plural to a singular
     print to_singular ('knives');
     # "knife"
 
-=cut
-package Lingua::EN::PluralToSingular;
-require Exporter;
-@ISA = qw(Exporter);
-@EXPORT_OK = qw/to_singular/;
-use warnings;
-use strict;
-our $VERSION = 0.01;
+=head1 DESCRIPTION
 
-my %plural = (
+This Perl module converts words denoting a plural in the English
+language into words denoting a singular noun.
 
-# us which are plural
-'menus' => 'menu',
-);
+=head1 METHODS
 
-my %not_plural = (
+=head2 to_singular
 
-);
+    my $singular = to_singular ($word);
 
-1;
+Convert C<$word> into its singular form. For example,
 
-__END__
+    to_singular ('cats')
+
+returns 'cat'. If the word is unknown or does not seem to be
+plural, C<to_singular> returns the word itself, so
+
+    to_singular ('battlehorn');
+
+returns 'battlehorn'.
 
 =head1 AUTHOR
 
--- t/Lingua-EN-PluralToSingular.t
+++ t/Lingua-EN-PluralToSingular.t
@@ -1,8 +1,24 @@
 use warnings;
 use strict;
-use Test::More tests => 1;
-BEGIN { use_ok('Lingua::EN::PluralToSingular') };
-use Lingua::EN::PluralToSingular;
+use Test::More tests => 10;
+BEGIN { use_ok('Lingua::EN::PluralToSingular', 'to_singular') };
+
+my %words = qw/ 
+bogus bogus 
+citrus citrus 
+menus menu 
+species species 
+flies fly 
+monkeys monkey 
+children child 
+women woman 
+mice mouse 
+/; 
+
+for my $word (sort keys %words) { 
+    my $s = to_singular ($word); 
+    ok ($s eq $words{$word}, "$s == $words{$word}"); 
+} 
 
 # Local variables:
 # mode: perl
